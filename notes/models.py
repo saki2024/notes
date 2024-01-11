0fcfe3886ac42cba1c38f3ee85155ee6f08819d0
saki2024 <sakiasikomaidrakulu@gmail.com>
2024-01-12 09:17:38 +1200

    added to admin

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class notes(models.Model):
+    text = models.TextField()
+    notes = models.TextField()
+    created = models.DateTimeField(auto_now_add = True)
